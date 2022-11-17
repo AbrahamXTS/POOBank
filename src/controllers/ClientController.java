3a481f4e76bceef453a6342b6195a78e4d1f540d
José Angel Magaña García <97571187+Xmahana@users.noreply.github.com>
2022-11-16 21:24:32 -0600

    Account Controller added

--- src/controllers/ClientController.java
+++ src/controllers/ClientController.java
@@ -2,18 +2,26 @@ package controllers;
 
 import services.ClientService;
 import utilities.WrapperResponse;
+import DTO.ClientDTO;
 
 public class ClientController {
     
     ClientService clientService;
 
+    public ClientController(ClientService cs){
+      this.clientService = cs;
+    }
+
     public WrapperResponse<Boolean> addClient(ClientDTO cdto) {
-        
+      return clientService.addClient(cdto);
+	  }
+
+    public WrapperResponse<Boolean> updateClient(ClientDTO cdto){
+      return clientService.updateClient(cdto);
+    }
+
+    public WrapperResponse<Boolean> deleteClient(int ID){
+      return clientService.deleteClient(ID);
+    }
     
-
-		return new WrapperResponse<Boolean>(null, null, null);
-	}
-
-
-
 }
---
+++ src/services/AccountService.java
@@ -0,0 +1,83 @@
+package services;
+
+import models.database.DB_query;
+import utilities.WrapperResponse;
+import DTO.AccountOneDTO;
+
+public class AccountService {
+    
+    DB_query query;
+
+    public AccountService(DB_query query) {
+        this.query = query;
+    }
+
+    public WrapperResponse<Boolean> addAccount(int ID) {
+        
+        boolean ok = false;
+        String message = "";
+        boolean added = false;
+        
+        try {
+            
+            query.NuevaCuenta(ID);
+            
+            ok = true;
+            message = "Account added correctly";
+            added = true;
+
+        } catch (Exception e) {
+
+            ok = false;
+            message = "Error, Account was not added";
+            added = false;
+        }
+
+		return new WrapperResponse<>(ok, message, added);
+	}
+
+    public WrapperResponse<Boolean> updateAccountBalance(int balance){
+        boolean ok = false;
+        String message = "";
+        boolean updated = false;
+
+        try {
+            query.ActualizarSaldo(balance, 1);
+            ok = true;
+            message = "Updated account balance";
+            updated = true;
+        }  catch (Exception e) {
+            ok = false;
+            message = "Account balance not updated :c";
+            updated = false;
+        }
+        
+        return new WrapperResponse<>(ok, message, updated);
+    }
+
+
+    public WrapperResponse<Boolean> deleteAccount(int ID_Cuenta){
+        
+        boolean ok = false;
+        String message = "";
+        boolean deleted = false;
+        String id_Cuenta = String.valueOf(ID_Cuenta);
+
+        try {
+
+            deleted = query.EliminarCuenta(id_Cuenta);
+
+            ok = true;
+            message = "Account was deleted";
+
+        } catch (Exception e){
+            
+            ok = false;
+            message = "Error, Account wasn't deleted";
+            deleted =  false; 
+        }
+                
+        return new WrapperResponse<Boolean>(ok, message, deleted);
+    }
+
+}
